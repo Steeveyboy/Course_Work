eefa9414483fe3245381fef9a8e6ff522cd697f2
Steeveyboy <jonathan.qsteeves@gmail.com>
2023-06-30 11:56:59 -0400

    Add rfr_calc
    Added a file to handle calculating the risk free rate

---
+++ Honours_Project/calcluations/rfr_calc.py
@@ -0,0 +1,4 @@
+"""This file will handle calculating the risk free rate for a given day over a period of time."""
+
+EX_RATES = {'2023-05-18', 5.59, 5.22, 5.31, 5.43, 5.38, 5.02, 4.24, 3.94, 3.69, 3.67, 3.65, 4.02, 3.91}
+
